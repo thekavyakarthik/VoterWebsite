f9adf07c9f9dd9b1dac4874d898c45538ca0fab3
MichaelFine2022 <mf9947967@gmail.com>
2024-10-24 08:49:35 -0700

    Created graph

---
+++ script.js
@@ -0,0 +1,19 @@
+// Data for the plot
+let trace1 = {
+    x: [1, 2, 3, 4, 5],
+    y: [10, 15, 13, 17, 12],
+    mode: 'lines',
+    name: 'Line Plot'
+  };
+
+  let data = [trace1];
+
+  // Layout configuration
+  let layout = {
+    title: 'Simple Line Plot',
+    xaxis: { title: 'X Axis Label' },
+    yaxis: { title: 'Y Axis Label' }
+  };
+
+  // Plot the graph in the 'myPlot' div
+  Plotly.newPlot('myPlot', data, layout);
